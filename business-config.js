ac56fa799425995b694b7f5b87320e1df579e2e8
HarshxMoonKnight <harshdas7826@gmail.com>
2026-04-20 22:24:13 +0530

    Add files via upload

---
+++ business-config.js
@@ -0,0 +1,41 @@
+window.CAFE_CONFIG = {
+  business: {
+    name: "Green-Wood Cafe",
+    shortName: "GW",
+    email: "hello@greenwoodcafe.com",
+    phoneDisplay: "+91 98765 43210",
+    phoneHref: "+919876543210",
+    whatsappNumber: "919871546439",
+    addressLines: [
+      "Plot No. 2, Pocket-6, Sector-A/10",
+      "Sector A10, Narela, Delhi, 110040",
+    ],
+    coordinates: {
+      lat: 28.7958763,
+      lng: 77.0984051,
+    },
+    mapQuery: "Plot No. 2, Pocket-6, Sector-A/10, Sector A10, Narela, Delhi, 110040",
+    mapEmbedUrl:
+      "https://www.google.com/maps?q=Plot%20No.%202%2C%20Pocket-6%2C%20Sector-A%2F10%2C%20Sector%20A10%2C%20Narela%2C%20Delhi%2C%20110040&z=15&output=embed",
+    footerTagline: "Your daily dose of comfort, one cup at a time.",
+    instagramUrl: "https://instagram.com/greenwoodcafe",
+    facebookUrl: "https://facebook.com/greenwoodcafe",
+    xUrl: "https://x.com/greenwoodcafe",
+  },
+  delivery: {
+    thresholdKm: 15,
+    longDistanceFee: 150,
+  },
+  storage: {
+    cartKey: "green-wood-cafe-cart",
+    adminSessionKey: "green-wood-cafe-admin-session",
+    siteSettingsCacheKey: "green-wood-cafe-site-settings",
+    menuCacheKey: "green-wood-cafe-menu-items",
+    menuImageBucket: "menu-images",
+    menuImageFolder: "menu-items",
+  },
+  supabase: {
+    url: "https://kbtmdtpdbufnikiaxpfq.supabase.co",
+    anonKey: "sb_publishable_NklKPE5SOrlcNAelPtys1w_zkUoR30x",
+  },
+};
